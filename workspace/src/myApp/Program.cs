a366978b6af7cd464397dc553275db29d52f4163
Jonas @ CoopX <jonas.blasas.lonnum@coop.no>
2020-06-17 15:47:23 +0200

    Initial commit

---
+++ workspace/src/myApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+
+            while (true)
+            {
+                continue;
+            }
+
+            Console.WriteLine("Hello world program exited");
+        }
+    }
+}
